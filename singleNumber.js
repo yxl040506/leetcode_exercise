e127c1c1f86ba6d87bb583e8830a38a1f20d60f9
liuxinyi12 <liuxinyi6@jd.com>
2020-11-06 10:51:58 +0800

    fix: 整理文件夹

--- singleNumber.js
+++
@@ -1,9 +0,0 @@
-var singleNumber = function(nums) {
-    let temp = 0
-    for(let i =0;i<nums.length; i++) {
-        temp ^= nums[i]
-    }
-    return temp
-};
-let result = singleNumber([4,1,2,1,2]);
-console.log(result)
--- tencent50/palinDrome.js
+++ tencent50/palinDrome.js
@@ -1,3 +1,4 @@
+// 是否回文数
 var isPalindrome = function(x) {
     if(x < 0 || (x !== 0 && x % 10 === 0)) {
         return false
